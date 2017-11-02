139d6e449365f761df91df014e3226a214687f24
jasonduquain <jduquain@gmail.com>
2017-11-01 23:23:12 -0400

    completed sticky hdr

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -43,17 +43,55 @@ class RevealOnScroll {
     }
 }
 
-
-
-
 var revealOnScroll = new RevealOnScroll();
 
 /******* StickyHeader.js************/
 
+class StickyHeader {
+    constructor() {
+        this.siteHeader = document.querySelector('.site-header');
+        this.headerTriggerElement = document.querySelector('.large-hero__title');
+        this.darkenHeader();
+        this.pageSections = document.querySelectorAll('.page-section');
+        this.navSections = document.querySelectorAll('.primary-nav a');
+        this.createPageSectHighlight(); 
+    }
+    
+    darkenHeader() {
+        document.addEventListener('scroll', () => {
+            if (this.headerTriggerElement.getBoundingClientRect().top <= 0) {
+               this.siteHeader.classList.add('site-header--dark');
+            } else {
+               this.siteHeader.classList.remove('site-header--dark');
+            }   
+        }); 
+    }
+    
+    createPageSectHighlight() {
+            this.pageSections.forEach((pageSect) => {
+                document.addEventListener('scroll', (e) => {    
+                    for (let i = 0; i < this.navSections.length; i++) {
+                        if (pageSect.getAttribute('data-matching-link').slice(1) ===    this.navSections[i].getAttribute('id')) {
+                            if (pageSect.getBoundingClientRect().top <= 0 &&             pageSect.getBoundingClientRect().bottom >= 0) {
+                               this.navSections[i].classList.add('is-current-link');
+                            } else {
+                                this.navSections[i].classList.remove('is-current-link');
+                            }
+                        }
+                    }
+            
+                });
+                    
+            });
+            
+           
+    }
+    
+}
 
 
-
-
-
+var stickyHeader = new StickyHeader();
 
 /************** Modal.js ***********/
+
+
